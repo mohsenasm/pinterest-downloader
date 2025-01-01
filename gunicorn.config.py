22f06d8213f316b6e2fba78e7613f7cd839b62ec
Mohammad Mohsen <m.m.asemanmanzar@gmail.com>
2025-01-01 17:15:41 +0330

    fix worker class

--- gunicorn.config.py
+++ gunicorn.config.py
@@ -1,13 +1,10 @@
-# file gunicorn.conf.py
-# coding=utf-8
-# Reference: https://github.com/benoitc/gunicorn/blob/master/examples/example_config.py
-
 import os
 
 loglevel = 'info'
 
 bind = '0.0.0.0:' + os.environ['PORT']
-workers = 1
+workers = 2
+worker_class = "app.TelegramBotUvicornWorker"
 
 timeout = 1 * 60  # 1 minutes
 keepalive = 24 * 60 * 60  # 1 day
