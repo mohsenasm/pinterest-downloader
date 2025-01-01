61a2f7e043aee2ff5faaed0f6b28462d267fa57d
Mohammad Mohsen <m.m.asemanmanzar@gmail.com>
2025-01-01 16:54:52 +0330

    load from secret.env

--- configs.py
+++ configs.py
@@ -1,4 +1,9 @@
 import os
+from dotenv import load_dotenv
+
+if os.path.isfile("secret.env"):
+    print("loading envs from secret.env")
+    load_dotenv("secret.env")
 
 BOT_ADMIN_ID = os.environ['BOT_ADMIN_ID']
 BOT_SECRET = os.environ['BOT_SECRET']
